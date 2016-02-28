553378641c4457731966016b9409ed52114bcfbe
Purushoth <purushothanbu@gmail.com>
2016-02-28 12:44:10 +0530

    [Bug Fix] Script forbidden error (403)

--- PdfMail/App_Start/BundleConfig.cs
+++ PdfMail/App_Start/BundleConfig.cs
@@ -7,13 +7,13 @@ namespace PdfMail
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new StyleBundle("~/Styles/app").Include(
+			bundles.Add(new StyleBundle("~/Styles/app-css").Include(
 				"~/Content/Site.css",
 				"~/Content/toastr.min.css",
 				"~/Content/nprogress.css"
 				));
 
-			bundles.Add(new ScriptBundle("~/Scripts/app").Include(
+			bundles.Add(new ScriptBundle("~/Scripts/app-js").Include(
 				"~/Scripts/vendor/jquery-1.12.1.min.js",
 				"~/Scripts/vendor/jspdf.min.js",
 				"~/Scripts/vendor/toastr.min.js",
